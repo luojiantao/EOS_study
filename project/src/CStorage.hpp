3eab7334645b0e3c8db54c5a7eadedf03ceca494
luojiantao <33904593+luojiantao@users.noreply.github.com>
2019-01-19 14:39:52 +0800

    Update CStorage.hpp

--- project/src/CStorage.hpp
+++ project/src/CStorage.hpp
@@ -1,22 +1,32 @@
 #pragma once
-#include "luo.hpp"
+#include <eosiolib/eosio.hpp>
+#include<string>
+#include<stdlib.h>
+use namespace std;
 namespace ContractStorage{
 
 //合约内容存储
 //@abi table ContractData i64
 struct ContractData {
     uint64_t creator;
-    uint64_t hash;// sha256(account + content)
+    uint64_t hash;// sha256(creator + content)
     string content;
     string UseInfo;//存储所有合约状态的索引
     //定义address表的主键
-    uint64_t primary_key() const { return creator; }
+    uint64_t primary_key() const { return hash; }
     //定义extractor，二级索引是phone
-    uint64_t get_hash() const {return hash; }
+    uint64_t get_creator() const {return creator; }
  
     //序列化
-    EOSLIB_SERIALIZE(ContractData, (creator)(hash)(content));
+    EOSLIB_SERIALIZE(ContractData, (creator)(hash)(content)(UseInfo));
 };
+
+typedef eosio::multi_index< N(ContractData), ContractData,
+            indexed_by<N(creator), const_mem_fun<ContractData, uint64_t, &ContractData::get_creator>>>
+    ContractData_index;
+    
+
+
 //合约状态存储
 //@abi table ContractState i64
 struct ContractState {
@@ -28,14 +38,17 @@ struct ContractState {
     //定义address表的主键
     uint64_t primary_key() const { return hash; }
     //定义extractor，二级索引是phone
-    uint64_t get_Sponsor() const {return Sponsor; }
+    uint64_t get_sponsor() const {return Sponsor; }
  
     //序列化
     EOSLIB_SERIALIZE(ContractData, (Sponsor)(hash)(describe)(State));
 };
 
-	class CStorage{
-		
+	typedef eosio::multi_index< N(ContractState), ContractState,
+            indexed_by<N(Sponsor), const_mem_fun<ContractState, uint64_t, &ContractState::get_sponsor>>>
+    ContractData_index;
+	
+	class CStorage{	
 	};
    //Storage();
   // GetContract();
