7b4189e1e7e880a9a609667d6da11706c4f133c6
jiantaoLuo <574608982@qq.com>
2019-01-22 23:46:02 +0800

    构建反馈回环

--- CreateENVFromEOS.sh
+++ CreateENVFromEOS.sh
@@ -13,3 +13,6 @@ cp $eos_path/libraries ./ -rf
 cp $eos_path/externals ./ -rf
 cp $eos_path/contracts ./ -rf
 cp ./ENV/appbase_comp.camke.in  ./libraries/appbase/CMakeLists.txt
+
+current_path=`pwd`
+ln -s ${current_path}/project ${current_path}/contracts/project
