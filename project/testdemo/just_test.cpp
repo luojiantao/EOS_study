cea582a7f338ddcf1b7576df97373a5b68616c2e
jiantaoLuo <574608982@qq.com>
2019-02-03 21:15:43 +0800

    Update just_test.cpp

--- project/testdemo/just_test.cpp
+++ project/testdemo/just_test.cpp
@@ -23,23 +23,23 @@ using namespace eosio;
 
 CONTRACT hello : public contract {
    public:
-      using contract::contract;
+    	using contract::contract;
 
-      ACTION hi( name nm );
-      ACTION check( name nm );
+     	ACTION hello::hi( name nm ) {
+    		print_f("Name : %\n", nm);
+   			auto test_obj = Mediation::CContract();
+	  	}
+      	ACTION hello::check( name nm ) {
+   			print_f("Name : %\n", nm);
+   			eosio::check(nm == "hello"_n, "check name not equal to `hello`");
+		}
 
       using hi_action = action_wrapper<"hi"_n, &hello::hi>;
       using check_action = action_wrapper<"check"_n, &hello::check>;
 };
 
-ACTION hello::hi( name nm ) {
-   print_f("Name : %\n", nm);
-   auto test_obj = Mediation::CContract();
-}
 
-ACTION hello::check( name nm ) {
-   print_f("Name : %\n", nm);
-   eosio::check(nm == "hello"_n, "check name not equal to `hello`");
-}
+
+
 
 EOSIO_DISPATCH( hello, (hi)(check) )
