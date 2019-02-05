39292daeac4ed2a1eecbfb0798d27324eae86392
jiantaoLuo <574608982@qq.com>
2019-02-05 11:12:53 +0800

    test

--- project/include/Contract.hpp
+++ project/include/Contract.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <eosiolib/eosio.hpp>
-
+#include "base_64.h"
 namespace Mediation{
 	using CheckNum = std::string;
 	using Msg = std::string;
@@ -30,6 +30,17 @@ namespace Mediation{
 
     	//test
     	CContract();
+    	CContract(account_name create, std::string context = std::string()):
+    	m_creater(create),m_context(context)
+    	{
+    		checksum calc_hash;
+			sha256( data, length, &calc_hash );
+			Base64 *base = new Base64();
+			std::string normal,encoded;
+			encoded = base->Encode(&calc_hash,sizeof(calc_hash));
+			print_f("Name : %s\n", encoded.c_str());
+			//eos_assert( calc_hash == hash, "invalid hash" );
+    	}
     	~CContract();
 	private:
 		ContractState StateCalculate();
@@ -39,7 +50,7 @@ namespace Mediation{
         Msg m_context;
         account_name m_creater;
 	    unsigned short m_max_member = 1;
-	    std::string m_hash;
+	    std::string m_hash;//hash(m_creater + m_context)
 	    std::string m_next_hash;
 	    int m_contract_value = 0;
 	    int m_sign_value = 0;
