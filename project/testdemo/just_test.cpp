352f4f0fa106b119123fa2359ae5de10aeabca04
jiantaoLuo <574608982@qq.com>
2019-02-06 14:44:06 +0800

    Update just_test.cpp

--- project/testdemo/just_test.cpp
+++ project/testdemo/just_test.cpp
@@ -47,24 +47,41 @@ CONTRACT hello : public contract {
       uint64_t owner;
       uint64_t primary_key() const { return owner; }
     }；
-
-    struct ContractData {
+    TABLE ContractData {
       uint64_t creator;
       uint64_t hash;// sha256(creator + content)
       string content;
-      string UseInfo;//存储所有合约状态的索引
+      string UseInfo;///存储所有合约状态的索引
 
       uint64_t primary_key() const { return hash; }
 
       uint64_t get_creator() const {return creator; }
-   
+
       //序列化
       EOSLIB_SERIALIZE(ContractData, (creator)(hash)(content)(UseInfo));
     };
+
+    struct ContractState {
+        uint64_t Sponsor;
+        uint64_t hash;// sha256(Sponsor + ContractData.hash + describe)
+        string describe;
+        string State;
+        
+        uint64_t primary_key() const { return hash; }
+       
+        uint64_t get_sponsor() const {return Sponsor; }
+     
+        //序列化
+        EOSLIB_SERIALIZE(ContractData, (Sponsor)(hash)(describe)(State));
+    };
+
     typedef eosio::multi_index< "testtab"_n, test_table > testtable_t;
-    typedef eosio::multi_index< "ContractData"_n, ContractData,
+    typedef eosio::multi_index< "contractdata"_n, ContractData,
             indexed_by< "creator"_n, const_mem_fun<ContractData, uint64_t, &ContractData::get_creator > >
             > ContractData_index;
+    typedef eosio::multi_index< "contractstate"_n, ContractState,
+            indexed_by<N(Sponsor), const_mem_fun<ContractState, uint64_t, &ContractState::get_sponsor>>
+            > ContractState_index;
 
     using hi_action = action_wrapper<"hi"_n, &hello::hi>;
     using check_action = action_wrapper<"check"_n, &hello::check>;
