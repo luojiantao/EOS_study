f625563af92e5471e31358f31b9fd1af094b0b50
jiantaoLuo <574608982@qq.com>
2019-02-06 11:45:23 +0800

    Update just_test.cpp

--- project/testdemo/just_test.cpp
+++ project/testdemo/just_test.cpp
@@ -18,29 +18,62 @@ EOSIO_ABI( hello, (hi) )
 
 #include <eosiolib/eosio.hpp>
 #include "Contract.hpp"
-#inlcude "ABIheader.hpp"
+#include "ABIheader.hpp"
 
 using namespace eosio;
 
 CONTRACT hello : public contract {
    public:
-    	using contract::contract;
+  	using contract::contract;
+    void init(){}
+   	ACTION hi( name nm ) {
+  		print_f("Name : %\n", nm);
+ 			auto test_obj = Mediation::CContract();
+      testtable_t ll(_self, _self.value);
+  	}
+    	ACTION check( name nm ) {
+ 			print_f("Name : %\n", nm);
+ 			eosio::check(nm == "hello"_n, "check name not equal to `hello`");
+	  }
 
-     	ACTION hi( name nm ) {
-    		print_f("Name : %\n", nm);
-   			auto test_obj = Mediation::CContract();
-	  	}
-      	ACTION check( name nm ) {
-   			print_f("Name : %\n", nm);
-   			eosio::check(nm == "hello"_n, "check name not equal to `hello`");
-		}
+    ACTION post_info(std::string msg){
+      print(msg);
+    }
 
-      using hi_action = action_wrapper<"hi"_n, &hello::hi>;
-      using check_action = action_wrapper<"check"_n, &hello::check>;
+    ACTION get_info(std::string msg){
+      print(msg);
+    }
+    TABLE test_table {
+      uint64_t owner;
+      uint64_t primary_key() const { return owner; }
+    }；
+
+    struct ContractData {
+      uint64_t creator;
+      uint64_t hash;// sha256(creator + content)
+      string content;
+      string UseInfo;//存储所有合约状态的索引
+
+      uint64_t primary_key() const { return hash; }
+
+      uint64_t get_creator() const {return creator; }
+   
+      //序列化
+      EOSLIB_SERIALIZE(ContractData, (creator)(hash)(content)(UseInfo));
+    };
+    typedef eosio::multi_index< "testtab"_n, test_table > testtable_t;
+    typedef eosio::multi_index< "ContractData"_n, ContractData,
+            indexed_by< "creator"_n, const_mem_fun<ContractData, uint64_t, &ContractData::get_creator > >
+            > ContractData_index;
+
+    using hi_action = action_wrapper<"hi"_n, &hello::hi>;
+    using check_action = action_wrapper<"check"_n, &hello::check>;
 };
 
 
 
 
 
-EOSIO_DISPATCH( hello, (hi)(check) )
+
+
+EOSIO_DISPATCH( hello, (hi)(check)(post_info)(get_info) )
